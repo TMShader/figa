d656dd1640d20d6cc22182656e183cc5465555a0
TMShader <github@tmshader.me>
2021-09-01 23:46:45 +0200

    Add Login Status to Header on All Pages

--- routes/models.js
+++ routes/models.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const auth = require("../middleware/auth");
+const cookieParser = require("cookie-parser");
 const router = express.Router();
+
+router.use(cookieParser())
+router.use((req, res, next) => auth(req, res, next))
+
 module.exports = router;
 
 router.get("/m/:model", (req, res) => {
-	res.render("model", { model: req.params.model });
+	const { username, loggedIn } = req.user
+	res.render("model", {
+		username, loggedIn,
+		model: req.params.model
+	});
 });
